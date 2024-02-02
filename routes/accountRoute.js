f36f53201cd58061b04ba6ce2ce41302b0e2f392
AndersonOrtizDiasJunior <anderson_ortiz@hotmail.com.br>
2024-02-02 08:23:46 -0300

    learning activity week 4

---
+++ routes/accountRoute.js
@@ -0,0 +1,16 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const accController = require("../controllers/accountController.js")
+const utilities = require("../utilities");
+const regValidate = require('../utilities/account-validation')
+
+router.get("/login", utilities.handleErrors(accController.buildLogin));
+
+router.get("/register", utilities.handleErrors(accController.buildRegister));
+
+router.post('/register',regValidate.registationRules(), 
+regValidate.checkRegData, 
+utilities.handleErrors(accController.registerAccount));
+
+module.exports = router;
